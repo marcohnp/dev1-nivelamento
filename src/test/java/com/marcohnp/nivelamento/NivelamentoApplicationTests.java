34b02749973329697c9e8e79beb90c422b33f1fd
Marco Henrique Nunes Pereira <marcoh@grupodimed.com.br>
2021-06-13 20:10:57 -0300

    Projeto enviado ao GitHub

---
+++ src/test/java/com/marcohnp/nivelamento/NivelamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcohnp.nivelamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NivelamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
